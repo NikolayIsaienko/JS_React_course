b961788fc1ad68b88db6da5eabe21b150ce0203c
Nicolas <56636524+NikolayIsaienko@users.noreply.github.com>
2022-06-04 20:27:08 +0200

    edits in timer

--- JS_step_8(Food)/js/script.js
+++ JS_step_8(Food)/js/script.js
@@ -37,18 +37,26 @@ window.addEventListener('DOMContentLoaded', () => {
     //Timer
     const deadLine = '2021-05-28';
 
-    function getTimeRemeining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor(t / (1000 * 60 * 60) % 24),
-            minutes = Math.floor((t / 1000 / 60) % 60),
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((t / 1000 / 60) % 60);
             seconds = Math.floor((t / 1000) % 60);
+        }
         return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
+            total: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
         };
     }
 
